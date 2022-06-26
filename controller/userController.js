a3056d8b75ed27b63c03dd665c440334667c3ed9
Zerubabel Kassahun <95906335+ZerubabelK@users.noreply.github.com>
2022-06-26 07:25:24 -0700

    new commit

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,10 @@
 const sessions = require('express-session')
 
-const {validationRules} = require('express')
+const { validationRules } = require('express-validator')
+
+const {encryptPass, decryptPass} = require('../lib/modules')
+
+const conn = require('../config/DB_Connection')
 
 module.exports.controllers = {
     userLoginPage: (req, res)=>{
@@ -8,13 +12,50 @@ module.exports.controllers = {
     },
 
     userLogin: (req, res)=>{
-        const error = validationRules
+        const error = validationRules(req.body)
     },
-    userSignup: (req, res)=>{
-        if(req.session.userID){
-            return res.render('pages/index', {data: req.session.userID})
+    userSignup: async(req, res, next)=>{
+        const error = validationRules(req.body)
+        const { body } = req.body
+
+        if (!error.isEmpty()) {
+            return res.render('auth/login', {error: error.array()[0].msg
+            });
         }
-        const user = req.body
+
+        try{
+            let query = "SELECT * FROM `users` WHERE `user_name`=?";
+            conn.query(query, [body.user_name], async(error, row)=>{
+			if (error)
+			{
+				console.log(error)
+				throw error
+			}
+			
+			if (row.length >= 1) {
+				return res.render('auth/login', {error: 'This Username already in use.'});
+			}})
+            
+            const hashedPass = await encryptPass(body.password)
+            query = 'insert into `users`(`first_name`, `last_name`, `email`, `user_name`, `password`, `phone`) values(?,?,?,?,?,?)'
+            conn.query(query, [body.first_name, body.last_name, body.email, body.user_name, body.password, body.phone], (error, rows)=>{
+                if(error)
+                {
+                    console.log (error);
+                    throw error;
+                }
+                
+                if (rows.affectedRows !== 1) {
+                    return res.render('auth/login', 
+                                        {error: 'Your registration has failed.'});
+                }
+
+                res.render("auth/login",
+                            {msg: 'You have successfully registered. You can Login now!'});
+             });		
+        }catch(err){
+
+        }
 
     }
 }
